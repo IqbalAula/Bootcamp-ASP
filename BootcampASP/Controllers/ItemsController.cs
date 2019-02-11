e3704997c60ef276f8c91023f8315a9646e96661
IqbalAula <iqbalaularahman@gmail.com>
2019-02-11 08:36:03 +0700

    Items + Javascript Sweet Alert Delete

--- BootcampASP/Controllers/ItemsController.cs
+++ BootcampASP/Controllers/ItemsController.cs
@@ -13,6 +13,7 @@ namespace BootcampASP.Controllers
     {
         // GET: Items
         MyContext _context = new MyContext();
+        ItemVM itemVM = new ItemVM();
         public ActionResult Index()
         {
             var Get = _context.Items.Where(x => x.IsDelete == false).ToList();
@@ -22,7 +23,8 @@ namespace BootcampASP.Controllers
         [HttpGet]
         public ActionResult Create()
         {
-            var GetSupplier = _context.Suppliers.Select(item => new SelectListItem
+            var GetCombo = _context.Suppliers.Where(x => x.IsDelete == false);
+            var GetSupplier = GetCombo.Select(item => new SelectListItem
             {
                 Text = item.Name,
                 Value = item.Id.ToString()
@@ -39,7 +41,11 @@ namespace BootcampASP.Controllers
             if (ModelState.IsValid)
             {
                 Item item = new Item();
+                item.Name = ItemVM.Name;
+                item.Stock = ItemVM.Stock;
+                item.Price = ItemVM.Price;
                 item.CreateDate = DateTimeOffset.Now.LocalDateTime;
+                item.Suppliers = _context.Suppliers.Find(ItemVM.Suppliers_id);
                 item.IsDelete = false;
                 _context.Items.Add(item);
                 _context.SaveChanges();
@@ -48,6 +54,93 @@ namespace BootcampASP.Controllers
             return View(ItemVM);
         }
 
+        [HttpGet]
+        public ActionResult Edit(int? Id)
+        {            
+            if (Id == null)
+            {
+                throw new NullReferenceException();
+            }
+            var Get = _context.Items.SingleOrDefault(x => x.Id == Id);
+            if (Get == null)
+            {
+                throw new NullReferenceException();
+            }
 
+
+            var GetCombo = _context.Suppliers.Where(x => x.IsDelete == false);
+            var GetSupplier = GetCombo.Select(item => new SelectListItem()
+            {
+                Selected = (item.Id == Id),
+                Text = item.Name,
+                Value = item.Id.ToString()
+            }).ToList();
+            ViewBag.Suppliers = GetSupplier;
+
+            foreach (var get in GetSupplier)
+            {
+                if (get.Value.Equals(Get.Suppliers.Id.ToString()))
+                {
+                    get.Selected = true;
+                    break;
+                }
+            }
+            itemVM.Name = Get.Name;
+            itemVM.Stock = Get.Stock;
+            itemVM.Price = Get.Price;
+            itemVM.Suppliers_id = Get.Suppliers.Id;
+            return View(itemVM);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ItemVM ItemVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var Get = _context.Items.Find(ItemVM.Id);
+                Get.Name = ItemVM.Name;
+                Get.Stock = ItemVM.Stock;
+                Get.Price = ItemVM.Price;
+                Get.CreateDate = DateTimeOffset.Now.LocalDateTime;
+                Get.Suppliers = _context.Suppliers.Find(ItemVM.Suppliers_id);
+                Get.UpdateDate = DateTimeOffset.Now.LocalDateTime;
+                _context.Entry(Get).State = System.Data.Entity.EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ItemVM);
+        }
+
+        //[HttpGet]
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        throw new NullReferenceException();
+        //    }
+        //    var Get = _context.Items.SingleOrDefault(x => x.Id == id);
+        //    if (Get == null)
+        //    {
+        //        throw new NullReferenceException();
+        //    }
+        //    return View(Get);
+        //}
+
+        //[HttpPost, ActionName("Delete")]
+        [HttpPost]
+        public ActionResult Delete(int? Id)
+        {
+            if (ModelState.IsValid)
+            {
+                var Get = _context.Items.SingleOrDefault(x => x.Id == Id);
+                Get.DeleteDate = DateTimeOffset.Now.LocalDateTime;
+                Get.IsDelete = true;
+                _context.Entry(Get).State = System.Data.Entity.EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
     }
 }
---
+++ BootcampASP/Scripts/SweetDelete.js
@@ -0,0 +1,31 @@
+﻿function deleteItem(Id) {
+    debugger;
+    swal({
+        title: "Are you sure?",
+        text: "Are you sure that you want to delete this Order?",
+        type: "warning",
+        showCancelButton: true,
+        closeOnConfirm: false,
+        confirmButtonText: "Yes, delete it!",
+        confirmButtonColor: "#ec6c62"
+    }, function () {
+        $.ajax({
+            url: "http://localhost:55403/Items/Delete/",
+            data: { "Id": Id },
+            type: "POST",
+            success: function (response) {
+                swal({
+                    title: "Deleted",
+                    text: "Items is deleted!",
+                    type: "success"
+                },
+                function () {
+                    window.location.href = '/Items/Index/';
+                });
+            },
+            error: function (response) {
+                swal("Oopss", "We couldn't connect to the server", "error")
+            }
+        });
+    });
+}
